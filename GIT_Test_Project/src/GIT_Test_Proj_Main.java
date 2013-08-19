4397b6bc0e5821921345276d31add6277ce522f4
Gregg Hanks <Gregg.Hanks@CummingsAerospace.com>
2013-08-19 13:26:52 -0500

    Initial Test Commit

---
+++ GIT_Test_Project/src/GIT_Test_Proj_Main.java
@@ -0,0 +1,12 @@
+
+public class GIT_Test_Proj_Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
